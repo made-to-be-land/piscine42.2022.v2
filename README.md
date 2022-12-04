d05d13dedc0ea17522a6e9d8ee3acab7a86a0163
Maksym <made-to-be-land>
2022-12-04 15:18:07 +0200

    100%

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # piscine42.2022.v2
+
+if you are allready here you now what to do.
---
+++ c02v2/ex09/ft_strcapitalize.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdiachen <mdiachen@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 13:25:21 by mdiachen          #+#    #+#             */
+/*   Updated: 2022/11/19 13:25:21 by mdiachen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcapitalize(char *str)
+{
+	int	c;
+	int	i;
+
+	c = 0;
+	i = 0;
+	while (str[c] != '\0')
+	{
+		if (i == 0 && (str[c] >= 'a' && str[c] <= 'z'))
+		{
+			str[c] -= 'a' - 'A';
+			i++;
+		}
+		else if (i > 0 && (str[c] >= 'A' && str[c] <= 'Z'))
+			str[c] += 'a' - 'A';
+		else if ((str[c] < '0') || (str[c] > '9' && str[c] < 'A')
+			|| (str[c] > 'Z' && str[c] < 'a') || (str[c] > 'z'))
+			i = 0;
+		else
+			i++;
+		c++;
+	}
+	return (str);
+}
